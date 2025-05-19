77d9dc2477e02e532dedc48156e18f4d7fd96871
dumbshadok <dumbshadok@proton.me>
2025-05-19 14:33:02 +0200

    reworked home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,22 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  constructor(private router: Router) {}
 
+  navigateToCookbook() {
+    this.router.navigate(['/cookbook']);
+  }
+
+  navigateToPantry() {
+    this.router.navigate(['/pantry']);
+  }
 }
